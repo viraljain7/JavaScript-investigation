214647918b1d35a1754f5cd497f0a3d735b50c32
viral jain <88425229+viraljain7@users.noreply.github.com>
2023-10-06 21:30:36 +0530

    Basic @.0

---
+++ 2.0_basic/01.0_Arrays.js
@@ -0,0 +1,32 @@
+// let arr1=[1,2,true,"viral"]
+// let arr2=new Array(1,2,4, true);
+//
+// console.log(arr2)
+// arr2.push(5) //Add elemnt to last
+// console.log(arr2)
+// arr2.pop() //Remove Elemnt to last
+// console.log(arr2)
+// arr2.unshift(9) //Add elemnt to First
+// console.log(arr2)
+// arr2.shift() //Remove elemnt to First
+// console.log(arr2)
+//
+//
+// const narr=arr2.join() //Join arr2 to narr and convert into string
+// console.log(narr)
+// console.log(typeof narr )
+
+let ary=[0,1,2,3,4,5,6]
+console.log("og arr : ",ary)
+let ary1=ary.slice(2,4)
+console.log("slice arr : ",ary1)
+console.log("og arr after slice: ",ary)
+ let ary2=ary.splice(2,4)
+console.log("splice arr : ",ary2)
+console.log("og arr after splice: ",ary)
+
+
+
+
+
+
---
+++ 2.0_basic/02.2_Object.js
@@ -0,0 +1,15 @@
+ // let course={
+ //    courseName:"JavaScript",
+ //     price:"2999",
+ //     CourseInstructor:"Hitesh Sir"
+ // }
+ //
+ // let {CourseInstructor:techer,price:pi}=course
+ //
+ // console.log(techer,pi)
+
+ // JSON File -json Formatter
+ // {
+ //     'name':"viral",
+ //     "sal":"1000"
+ // }
