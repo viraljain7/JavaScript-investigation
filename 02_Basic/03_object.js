2b70c95b0bd8af3c80310aee1a570b05371e6a1c
viraljain7 <veerljain1234@gmail.com>
2024-02-24 16:33:13 +0530

    updated

--- 02_Basic/03_object.js
+++ 02_Basic/03_object.js
@@ -1,32 +1,31 @@
-// singleton
+// singleton - constructor 
 // Object.create
 
 // Object Literals
-const mysymbol=Symbol("abc") //must Step 
-const JsUser={
-name:"viral",
-EmpId:20,
-"Full name":"viral jain",
-salary:20000,
-lastLoggedIn:["Mon","Tue"],
-[mysymbol]:"symbol",//symbol,
+const mysymbol = Symbol("abc") //must to declare inorder to use in object
 
-greeting: function () {
-    console.log('====================================');
-    console.log(`${this.name}`);
-    console.log('====================================');  
-}
+const JsUser = {
+    [mysymbol]: "myownkey",//symbol <---important--->
+    name: "viral",
+    "Full name": "viral jain",
+    lastLoggedIn: ["Mon", "Tue"],
+
+    greeting: function () {
+        console.log('====================================');
+        console.log(`${this.name}`);
+        console.log('====================================');
+    }
 }
 
 console.log(JsUser.name);
-console.log(JsUser.EmpId);
 console.log(JsUser["Full name"]);
 console.log(JsUser[mysymbol]);
 console.log(typeof JsUser[mysymbol]);
 console.log(JsUser.greeting());
 
 
-JsUser.name="abc"
-Object.freeze(JsUser)
-JsUser.name="abcdee"
+JsUser.name = "abc"
+Object.freeze(JsUser)//used to freezed object
+
+JsUser.name = "abcdee"// not able to change object property
 console.log(JsUser);
