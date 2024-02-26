e227cd9aa3c3405bbaa038a1d54a4415965d5af6
viraljain7 <veerljain1234@gmail.com>
2024-02-26 12:17:14 +0530

    updated

--- 05_Loop/04_HOfunction.js
+++ 05_Loop/04_HOfunction.js
@@ -2,26 +2,26 @@
 
 let arr = [1, 2, 3, "viral", true]
 let arr2 = [1, 2, 3, "viralj", true]
-arr.forEach((value, key, arr) => {
-    console.log(key, " :- ", value, arr);
+arr2.forEach((value, key, givenArr) => {
+    console.log(key, " :- ", value, givenArr);
 })
 
 
 const obj1 = [{
-        "lang": "a",
-        "id": "b,",
-        "code": "c"
-    },
-    {
-        "lang": "aa",
-        "id": "bb,",
-        "code": "cc"
-    },
-    {
-        "lang": "aaa",
-        "code": "ccc"
-    }
+    "lang": "a",
+    "id": "b,",
+    "code": "c"
+},
+{
+    "lang": "aa",
+    "id": "bb,",
+    "code": "cc"
+},
+{
+    "lang": "aaa",
+    "code": "ccc"
+}
 ]
-obj1.forEach((value,key)=> {
-    console.log(key," :-",value.id);
+obj1.forEach((value, key) => {
+    console.log(key, " :-", value.id);
 });
