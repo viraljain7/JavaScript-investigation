897abfa335ee3697260d1c9ebebddf7f99f8d9e8
viraljain7 <veerljain1234@gmail.com>
2024-02-26 13:00:31 +0530

    updated

--- 05_Loop/05_filter_map_reduce.js
+++ 05_Loop/05_filter_map_reduce.js
@@ -44,12 +44,7 @@ console.log(reduce);
 
 
 
-const myNums = [1, 2, 3]
 
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
-// }, 0)
 
 const myTotal = myNums.reduce((acc, curr) => acc + curr, 0)
 
